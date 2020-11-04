37b905089dc282a04e0fc2d73f5f8a22921d2cb5
jacobortiz <54605823+jacobortiz@users.noreply.github.com>
2020-11-03 17:41:08 -0800

    fixing pods

--- Pods/Target Support Files/Pods-FoodForever-FoodForeverUITests/Pods-FoodForever-FoodForeverUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodForever_FoodForeverUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FoodForever_FoodForeverUITests
-@end
--- Pods/Target Support Files/Pods-FoodForever/Pods-FoodForever-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodForever : NSObject
-@end
-@implementation PodsDummy_Pods_FoodForever
-@end
